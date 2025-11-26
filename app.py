7256d629d9999ca4089a8b95fba9fe84a04f9b5e
Jasmine Medina Ramos <ramosjasmine670@gmail.com>
2025-11-27 05:09:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,10 @@ from .database import Database
 from .main_ui import MainApp
 from .dialogs import LoginDialog, PolicyDialog
 
-# Startup script
-
 def run_app():
     db = Database()
     app = MainApp(db)
 
-    # Show policy and login dialogs before mainloop
     pol = PolicyDialog(app)
     app.wait_window(pol)
     if not pol.accepted:
@@ -22,7 +19,6 @@ def run_app():
         return
 
     app.mainloop()
-
 
 if __name__ == '__main__':
     run_app()
